fc264be40edf13af99cf61865d58eb07ecaf1dfa
ivansabart <ivansabart@gmail.com>
2018-11-11 17:18:09 -0600

    Firts WebSite Upload

---
+++ Index.php
@@ -0,0 +1,28 @@
+<?php
+//$titulo = 'Masou Shoujo';
+include_once 'plantilla/documento-declaracion.inc.php';
+include_once 'plantilla/navbar.inc.php';
+?>   
+        
+        <div  class="container">
+            <div class="jumbotron">
+                <h1>Magical Lyrical Masou Shoujo Electronical Arts Infinity Climax Reloaded Beat Up Strem II</h1>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="panel panel default">
+                    <div class="panel-heading">
+                    <span class="glyphicon glyphicon-film" aria-hidden="true"></span>Video  
+                    </div>
+                    <div class="panel-body">
+                        <p>Video</p> 
+                        <video src="#"></video>     
+                    </div>
+                    </div>
+                </div>              
+            </div>
+        </div>
+
+<?php
+include_once 'plantilla/documento-cierre.inc.php';
+?>
---
+++ Plantilla/navbar.inc.php
@@ -0,0 +1,21 @@
+<nav class="navbar-default navbar-static-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">Este botón despliega la barra de navegación</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">Masou Shoujo</a>
+                </div>
+                <div id="navbar" class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="index.php"><span class="glyphicon glyphicon-home" aria-hidden="true"></span>Home</a></li>
+                        <li><a href="game.php"><span class="glyphicon glyphicon-star" aria-hidden="true"></span>Game</a></li>
+                        <li><a href="company.php"><span class="glyphicon glyphicon-check" aria-hidden="true"></span>Company</a></li>
+                        <li><a href="guia.php"><span class="glyphicon glyphicon-book" aria-hidden="true"></span>Guia</a></li>
+                    </ul>
+                </div>
+            </div> 
+</nav>
